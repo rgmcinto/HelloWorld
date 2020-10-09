bcf1f3a0c72989295672b2b1d99a12911c7fdba8
Robert McIntosh <rgmcinto@asu.edu>
2020-10-09 10:38:47 -0700

    actual functionality changed

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -10,6 +10,16 @@ namespace GitTest
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("\nHello Robert\n\nEnter Your Age");
+
+            string inp = Console.ReadLine();
+
+            int age = Convert.ToInt32(inp);
+
+            Console.WriteLine("Your Age is {0}", age);
+            Console.ReadLine();
+
         }
     }
 }
